11567e6e959330c6e3d757f6acf2dd32e8e2ecf8
Edgarsar <edgarsargsyan@hotmail.com>
2022-03-28 03:23:04 +0000

    Add an event to the scroll top botton

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-  
+
   const $textarea = $("#tweet-text")
-  
+
   $textarea.on("input", function () {
-   const $output = $($(this)[0].form[2]);
+    const $output = $($(this)[0].form[2]);
     const len = $(this).val().length;
-    
+
     if (len > 140) {
       $output.css("color", "#ff0000");
     } else {
@@ -14,4 +14,42 @@ $(document).ready(function () {
     $output.html(140 - len);
 
   });
+
+
+
+  // makes the form slide up when the 'new' button in the navigation is clicked
+  $('#slide-up').click(function () {
+    //enable the text area
+    $("#tweet-text").focus();
+    //scroll the page to the top
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+    return false;
+  })
+
+
+  // makes the page scroll to the top and enable the textarea when the scroll top button is clicked
+  $(window).scroll(function () {
+      // the bottom button disappears when the user scrolls back to the top
+    if ($(this).scrollTop() <= 100) {
+      $('#scroll-top').fadeOut();
+    } else {
+      // the bottom button appears when the user starts to scroll
+      $("#scroll-top").css("display", "inline")
+    }
+  });
+  
+  // when scroll to the top button is clicked
+  $('#scroll-top').click(function () {
+    // enable the text area
+    $("#tweet-text").focus();
+    // scroll the page to the top
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+    return false;
+  })
+
+
 });
