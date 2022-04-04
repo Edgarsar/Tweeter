f971e66cefab2c3f4e5d7bfb20a837c3130778e7
Edgarsar <edgarsargsyan@hotmail.com>
2022-04-04 02:05:32 +0000

    Remove scroll to top function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -17,41 +17,4 @@ $(() => {
 
   });
 
-
-
-  // makes the form slide up when the 'new' button in the navigation is clicked
-  $('#slide-up').click(function() {
-    //enable the text area
-    $("#tweet-text").focus();
-    //scroll the page to the top
-    $("html, body").animate({
-      scrollTop: 0
-    }, "slow");
-    return false;
-  });
-
-
-  // makes the page scroll to the top and enable the textarea when the scroll top button is clicked
-  $(window).scroll(function() {
-    // the bottom button disappears when the user scrolls back to the top
-    if ($(this).scrollTop() <= 100) {
-      $('#scroll-top').fadeOut();
-    } else {
-      // the bottom button appears when the user starts to scroll
-      $("#scroll-top").css("display", "inline");
-    }
-  });
-
-  // when scroll to the top button is clicked
-  $('#scroll-top').click(function() {
-    // enable the text area
-    $("#tweet-text").focus();
-    // scroll the page to the top
-    $("html, body").animate({
-      scrollTop: 0
-    }, "slow");
-    return false;
-  });
-
-
 });
